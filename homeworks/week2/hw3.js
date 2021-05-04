4662a0fc98b5bb228900d265b7c1651053c0a2e6
Kevin <kevindtrt@gmail.com>
2021-05-04 19:16:19 +0800

    hwok

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
 function reverse(str) {
-  
+    let strArr = []
+    let newstr = ''
+    for (let i=str.length ; i>=0; i--) {
+      strArr.push(str[i])
+          newstr += str[i]
+    }
+    console.log(newstr)  
+    // 可以順利把字串反轉，但是不知道為何會有undefine 出現，求開示
 }
 
 reverse('hello');
