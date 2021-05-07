c101d1757bb00f97ef4bb1cf7ead5ad39511ac8c
kevindtrt <82004758+kevindtrt@users.noreply.github.com>
2021-05-07 14:09:51 +0800

    Update hw2.js

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -2,7 +2,6 @@ function capitalize(str) {
     let newstr = ''
     if (str[0]>= 'a' && str[0]<= 'z') {
          newstr = str.replace(str[0], String.fromCharCode(str.charCodeAt(0) - 32)) 
-    
          return newstr    
     } else return str   
     
