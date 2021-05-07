06a21779b42e739454e4931498f7538437967876
kevindtrt <82004758+kevindtrt@users.noreply.github.com>
2021-05-07 14:10:28 +0800

    Update hw3.js

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -2,10 +2,10 @@ function reverse(str) {
     let newstr = ''
     let x = str.length -1
     for (let i= x ; i>=0; i--) {
-      newstr += str[i]
+        newstr += str[i]
     }
     console.log(newstr)  
-    //return 1
+    return 1
     
 }
 
