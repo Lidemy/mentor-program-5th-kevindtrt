2b815d2769e24920ba03bf00c0c15e6695084251
kevindtrt <82004758+kevindtrt@users.noreply.github.com>
2021-05-07 14:12:02 +0800

    Update hw5.js

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,20 +1,18 @@
 function join(arr, concatStr) {
-  let str = ''
-  for (let i=0; i<arr.length ; i++) {
-    str += arr[i]
-    if (i < arr.length -1) str += concatStr
-  }
-  return str
-  
+    let str = ''
+    for (let i=0; i<arr.length ; i++) {
+        str += arr[i]
+        if (i < arr.length -1) str += concatStr
+    }
+    return str
 }
 
 function repeat(str, times) {
-  let repeatStr = ''
-  for (let i = 0; i < times; i++) {
-    repeatStr += str
-  }
-  return repeatStr
-  
+    let repeatStr = ''
+    for (let i = 0; i < times; i++) {
+        repeatStr += str
+    }
+    return repeatStr
 }
 
 console.log(join(['a'], '!'));
