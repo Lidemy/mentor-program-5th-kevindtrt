b30d5838733e1c7b3639b6869b8b4afa09351107
unknown <kevindtrt@gmail.com>
2021-05-11 10:02:45 +0800

    051101

---
+++ homeworks/week3/test.js
@@ -0,0 +1,18 @@
+// const n = 2
+
+// console.log(`${n} x 1 = ${n * 1}`)
+for (let k = 2; k < 10; k = k + 4) {
+  // console.log('last = ' + k)
+  for (let j = 1; j < 10; j++) {
+    let strLine = ''
+    for (let i = k; i < k + 4; i++) {
+      // console.log('first1 =' + k)
+      // console.log(`${i} x 1 = ${i * 1}`)
+      const strTmp = `${i} x ${j} = ${i * j}   `
+      strLine += strTmp
+      // console.log(strTmp)
+    }
+    console.log(strLine)
+  }
+  console.log('\n')
+}
