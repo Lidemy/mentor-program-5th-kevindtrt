f0cfaa553a81f6e0b33cd102db84cd2be0dcedbd
unknown <kevindtrt@gmail.com>
2021-05-12 15:54:32 +0800

    051204

--- homeworks/week3/code.js
+++ homeworks/week3/code.js
@@ -14,13 +14,5 @@ rl.on('close', () => {
 })
 
 function solve(input) {
-  const str1 = input[0]
-  const str2 = str1.split('').reverse().join('')
-  const center = Math.floor(str1.length / 2)
-  for (let i = 0; i < center; i++) {
-    if (str1[i] !== str2[i]) {
-      return console.log('False')
-    }
-  }
-  return console.log('Ture')
+
 }
