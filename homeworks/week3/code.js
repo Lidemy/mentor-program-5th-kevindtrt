6e8d36ca0a984a1d58712ef327817224a02f5d07
unknown <kevindtrt@gmail.com>
2021-05-12 11:16:39 +0800

    051201

--- homeworks/week3/code.js
+++ homeworks/week3/code.js
@@ -14,28 +14,42 @@ rl.on('close', () => {
 })
 
 function solve(input) {
-  const tmp = input[0].split(' ')
-  const n = Number(tmp[0])
-  const m = Number(tmp[1])
-  if (n < 1 || m > 1000000) {
-    return console.log('over')
-  }
-
-  let narcissisticNumber = ''
-  for (let i = n; i <= m; i++) {
-    if (i === checkbox(i)) {
-      narcissisticNumber += `${i} + '\n'`
+  const arrCount = []
+  arrCount.push(input[0])
+  const arr = []
+  for (let i = 1; i < input.length - 1; i++) {
+    arr.push(input[i])
+  } // 17-22:擷取 input 資料
+  // console.log('step2')
+  if (Number(arrCount[0]) < 1 || Number(arrCount[0]) > 100) {
+    return console.log('N Out of Range')
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < 1 || arr[i] > 1000000) {
+        return console.log('P Out of Range')
+      }
     }
   }
-  console.log(narcissisticNumber)
+  for (let i = 0; i < arr.length; i++) {
+    console.log(modCheck(Number(arr[i])))
+  }
 }
 
-function checkbox(number) {
-  let str = ''
-  str = str + number
-  let numbTmp = 0
-  for (let i = 0; i < str.length; i++) {
-    numbTmp = numbTmp + (Number(str[i]) ** str.length)
+function modCheck(number) {
+  const n = number
+  const end = Math.ceil(n / 2)
+  let mod = 0
+
+  for (let i = 2; i <= end; i++) {
+    if (n % i === 0) {
+      mod++
+    }
   }
-  return numbTmp
+  if (n === 1) {
+    return 'Composite'
+  } else if (mod < 1) {
+    return 'Prime'
+  } else {
+    return 'Composite'
+  }
 }
